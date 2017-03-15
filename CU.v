37790d3ec29e2bcc1d366eb2d39aca6c56e3468f
Joe <joao.euu@gmail.com>
2017-03-15 16:48:56 -0300

    control unit start

--- CU.v
+++ CU.v
@@ -1,17 +1,32 @@
 // [[Joe - joao.euu@gmail.com]]
 
-module ControlUnit (instruction,opcode,op1,op2,imControl,regenable,ramenable,pcControl,writecode);
+module ControlUnit (clock,instruction,opcode,op1,op2,imControl,regenable,ramenable,pcControl);
+  input clock;
   input [31:0] instruction;
 
   output [5:0] opcode;
   output [4:0] op1;
   output [4:0] op2;
-  output imControl;
-  output regenable;
-  output ramenable;
-  output pcControl;
-  output writecode;
+  output reg imControl;
+  output reg regenable;
+  output reg ramenable;
+  output reg pcControl;
 
-  localparam [4:0] ADD = 6'd0, SUB=6'd1,MUL=6'd2;
+  localparam [4:0] ADD = 6'd0,SUB = 6'd1,MUL = 6'd2;
+
+  assign opcode = instruction[31:25];
+  assign op1 = instruction[25:20];
+  assign op2 = instruction[20:0];
+
+  always @(*) begin
+    case(opcode)
+      ADD:begin
+        imControl=1'd0;
+        regenable=1'd0;
+        ramenable=1'd0;
+        pcControl=1'd0;
+      end
+    endcase
+  end
 
 endmodule
