78266185c023a5ab77cdbf7b918a4c9df59fcb22
JoaoLVN <joao.euu@gmail.com>
2017-04-21 21:20:07 -0300

    Added Items

---
+++ Atom Plugin/simple-assembler/spec/simple-assembler-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SimpleAssemblerView from '../lib/simple-assembler-view';
+
+describe('SimpleAssemblerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
